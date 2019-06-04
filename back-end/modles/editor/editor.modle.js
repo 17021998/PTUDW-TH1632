738485d627044efa210b7a2b94e07fb6872eabc5
sangnguyen <vansangnguyen1998@gmail.com>
2019-06-04 18:06:28 +0700

    update xet duyet bai viet va lay conten editor

--- back-end/modles/editor/editor.modle.js
+++ back-end/modles/editor/editor.modle.js
@@ -5,11 +5,23 @@ var nametable = "catpost";
 module.exports = {
 
     allPostByCatagory: CatID=>{
-        return db.load(`select p.*, c.CatName from post as p, catpost as cp , catagory as c where p.ID = cp.PostID and cp.CatID = c.ID and cp.CatID = ${CatID}`);
+        return db.load(`select p.*, c.CatName from post as p, catpost as cp , catagory as c where p.ID = cp.PostID and cp.CatID = c.ID and cp.CatID = ${CatID} and PostStatus is null`);
     },
 
     allCatagory: ()=>{
         return db.load('select c.ID, c.CatName from catagory as c where c.SuperCatID is null');
+    },
+
+    getContentPost: ID=>{
+        return db.load(`select p.Content from post as p where p.ID = ${ID}`);
+    },
+
+    getIDCatagoryByPostID: Postid=>{
+        return db.load(`select cp.CatID from catpost as cp where cp.PostID = ${Postid}`);
+    },
+
+    xetDuyetPost: entity=>{
+        return db.update('post','ID', entity);
     }
     
 };
