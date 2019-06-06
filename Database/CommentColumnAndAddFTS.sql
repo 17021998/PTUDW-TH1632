6f9c226ed943643ff75946bc24ca008a5ef4707e
Quang <ntquang17021998@gmail.com>
2019-06-06 19:30:12 +0700

    them table comment

---
+++ Database/CommentColumnAndAddFTS.sql
@@ -0,0 +1,21 @@
+USE `th16news`;
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `ID` varchar(50) NOT NULL,
+  `PostID` int(11) NOT NULL,
+  `UserID` varchar(50) DEFAULT NULL,
+  `Content` text NOT NULL,
+  `IsDelete` int(1) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `comment`
+  ADD CONSTRAINT `fk_comm_post` FOREIGN KEY (`PostID`) REFERENCES `post` (`ID`);
+
+ALTER TABLE `comment`
+  ADD CONSTRAINT `fk_comm_user` FOREIGN KEY (`UserID`) REFERENCES `userprimary` (`ID`);
+
+ALTER TABLE `tag` ADD FULLTEXT KEY (`TagName`);
+
+ALTER table `userprimary` ADD FULLTEXT KEY (`FullName`);
